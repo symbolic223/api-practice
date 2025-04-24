d198288e5a3bc9bed69f9f05ca8b6588518b6457
symbolic223 <danildemin00@gmail.com>
2025-04-24 09:28:44 +0700

    wtf

---
+++ script.js
@@ -0,0 +1,13 @@
+async function generateDiagram() {
+    const type = document.getElementById('diagramType').value;
+    const text = document.getElementById('diagramText').value;
+
+    const response = await fetch(`https://kroki.io/${type}/svg`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/plain' },
+        body: text
+    });
+
+    const svg = await response.text();
+    document.getElementById('result').innerHTML = svg;
+}
